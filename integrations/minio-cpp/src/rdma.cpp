da06de3950c86b9505ce1a52752ddf987d2c37ad
Jihyeon Gim <potatogim@potatogim.net>
2026-09-14 01:41:29 +0900

    minio-cpp: order the callback context initializer to match the fields
    Signed-off-by: Jihyeon Gim <potatogim@potatogim.net>

--- integrations/minio-cpp/src/rdma.cpp
+++ integrations/minio-cpp/src/rdma.cpp
@@ -991,8 +991,8 @@ int v2SendCancel(void* ctx, const hipObjTransferReqV2_t* req) {
 
 ssize_t rdmaPutV2(S3RdmaContext* sctx, void* buf, size_t size) {
   V2CallbackCtx cbctx{
-      sctx, clientNic(), {}, false, {},
-      hipObjSelectedPortV2(), hipObjSelectedGidIndexV2()};
+      sctx, clientNic(), hipObjSelectedPortV2(),
+      hipObjSelectedGidIndexV2(), {}, false, {}};
   hipObjOpsV2_t ops{};
   ops.sendPrepare = v2SendPrepare;
   ops.sendReadyRequest = v2SendReadyRequest;
@@ -1021,8 +1021,8 @@ ssize_t rdmaPutV2(S3RdmaContext* sctx, void* buf, size_t size) {
 
 ssize_t rdmaGetV2(S3RdmaContext* sctx, void* buf, size_t size) {
   V2CallbackCtx cbctx{
-      sctx, clientNic(), {}, false, {},
-      hipObjSelectedPortV2(), hipObjSelectedGidIndexV2()};
+      sctx, clientNic(), hipObjSelectedPortV2(),
+      hipObjSelectedGidIndexV2(), {}, false, {}};
   hipObjOpsV2_t ops{};
   ops.sendPrepare = v2SendPrepare;
   ops.sendReadyRequest = v2SendReadyRequest;
